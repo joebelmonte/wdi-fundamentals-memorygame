048447cd728e5aed9015ba201c3ba0191049f594
Joe Belmonte-WDI <joebelmonte-wdi@Josephs-MacBook-Pro.local>
2017-03-23 22:05:51 -0400

    created game logic

--- JS/main.js
+++ JS/main.js
@@ -13,22 +13,25 @@ console.log("User flipped " + cardThree);
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[2];
-
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[3];
-
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
+var checkForMatch = function (){
+		if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.")
 	}
 }
+
+var flipCard = function (cardID){
+
+console.log("User flipped " + cards[cardID]);
+
+cardsInPlay.push(cards[cardID]);
+
+if (cardsInPlay.length === 2) {
+	checkForMatch();
+}	
+
+}
+
+flipCard(0);
+flipCard(2);
