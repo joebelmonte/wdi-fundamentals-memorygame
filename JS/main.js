0f6baed6155a696acdeb8cb19db115ab35a2d8c4
yourUsername <joseph.belmonte@gmail.com>
2017-03-26 22:40:12 -0400

    created game logic

--- JS/main.js
+++ JS/main.js
@@ -45,7 +45,8 @@ var checkForMatch = function (){
 	}
 }
 
-var flipCard = function (cardID){
+var flipCard = function (){
+	var cardID = this.getAttribute('data-id');
 
 	console.log("User flipped " + cards[cardID].rank);
 	console.log(cards[cardID].cardImage);
@@ -53,11 +54,21 @@ var flipCard = function (cardID){
 
 	cardsInPlay.push(cards[cardID].rank);
 
+	this.setAttribute('src', cards[cardID].cardImage);
+
 	if (cardsInPlay.length === 2) {
 	checkForMatch();
 	}	
 }
 
-flipCard(0);
-flipCard(1);
+var createBoard = function() {
+	  for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+  }
+}
 
+createBoard();
